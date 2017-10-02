4caadc52dd1a89c7934df86f0d95ef23b5157d2d
Zarak Mahmud <zarak.mahmud@utoronto.ca>
2017-10-03 01:08:39 +0500

    Add tests for invalid or missing data

--- datapoint/requests_tests.py
+++ datapoint/requests_tests.py
@@ -1,11 +1,7 @@
 import requests
-
 from unittest.mock import Mock, patch
-from urllib.parse import urljoin
 
-
-BASE_URI = ''
-BRENT_URI = urljoin(BASE_URI, 'BRENT/m')
+BASE_URI = 'api/datapoints'
 
 data = [{
     'name': 'BRENT',
@@ -13,6 +9,8 @@ data = [{
     'date': '2018-01-01',
     'value': 42.0
 }]
+
+payload = {'name': 'BRENT', 'freq': 'm'}
     
 
 @patch('requests.get')
@@ -20,7 +18,7 @@ def test_request_response(mock_get):
     """Should complete successfully with OK status code"""
     mock_get.return_value = Mock(ok=True)
 
-    response = requests.get(BRENT_URI)
+    response = requests.get(BASE_URI, params=payload)
     assert response.ok
 
 @patch('requests.get')
@@ -29,10 +27,28 @@ def test_getting_BRENT(mock_get):
     mock_get.return_value = Mock(ok=True)
     mock_get.return_value.json.return_value = data
 
-    response = requests.get(BRENT_URI)
+    response = requests.get(BASE_URI, params=payload)
     assert response.json()[0]['name'] == 'BRENT'
+
+@patch('requests.get')
+def test_request_with_empty_name(mock_get):
+    """Should return 400 error with empty name parameter"""
+    payload = {'name': '', 'freq': 'm'}
+    mock_get.return_value = Mock(status_code=400)
+
+    response = requests.get(BASE_URI, params=payload)
+    assert response.status_code == 400
+
+@patch('requests.get')
+def test_getting_data_not_found(mock_get):
+    """Should return response with empty json"""
+    payload = {'name': 'FOOBAR', 'freq': 'm'}
+    mock_get.return_value = Mock(ok=True)
+    mock_get.return_value.json.return_value = {}
+
+    response = requests.get(BASE_URI, params=payload)
+    assert response.json() == {}
 
 # @patch('requests.get')
 # def test_putting_data(mock_get):
-
-
+# [{ "date": "1999-03-31", "freq": "q", "name": "INVESTMENT_bln_rub", "value": 12345.6 }]
